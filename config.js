2959e25432eb50200b9920e391690049423c3b05
kimchuna <32409960+kimchuna@users.noreply.github.com>
2020-12-05 17:19:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__PAW_CONFIG__ = {
-    googleApiKey: 'AIzaSyBoX_m8IQZO2Fq__4XgkIRADhVXTJwtsbs',
+    googleApiKey: '240c16cedaf36e78fa7a64cbfb796f03d4df0ae0',
     googleSheetDocId: '1FTQkXswtDkoL3eeXAq3suBBfmNwV0oh7kXBnYQVUnns',
 }
